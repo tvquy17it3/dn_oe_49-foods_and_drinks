1b014b8853e9a834587119fec055481062b42779
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2021-10-13 21:19:40 +0700

    Layout Admin & I18n

---
+++ app/controllers/admin/admins_controller.rb
@@ -0,0 +1,5 @@
+class Admin::AdminsController < ApplicationController
+  layout "admin"
+
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    namespace :admin do
+      root "admins#index"
+    end
+  end
 end
