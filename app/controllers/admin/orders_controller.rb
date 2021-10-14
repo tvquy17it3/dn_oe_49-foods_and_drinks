22264e338ca9434679f73cda4a2925911d29fc8d
tvquy17it3 <tvquy.17it3@sict.udn.vn>
2021-10-14 15:06:48 +0700

    [admin] show order details

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,6 @@
 class Admin::OrdersController < Admin::AdminsController
+  before_action :load_order, only: :show
+
   def index
     @title = t "orders.all"
     @orders = Order.includes(:order_details, :user)
@@ -11,7 +13,21 @@ class Admin::OrdersController < Admin::AdminsController
     load_orders params[:status]
   end
 
+  def show
+    @title = t "orders.order_detail"
+    @order_details = @order.order_details.includes(:product)
+    @total = total_order @order_details
+  end
+
   private
+
+  def load_order
+    @order = Order.find_by(id: params[:id])
+    return if @order
+
+    flash[:danger] = t "orders.no_order"
+    redirect_to admin_orders_path
+  end
 
   def load_orders status
     if Order.statuses.include?(status)
@@ -23,6 +39,12 @@ class Admin::OrdersController < Admin::AdminsController
     else
       flash[:success] = t "error_path"
       redirect_to admin_orders_path
+    end
+  end
+
+  def total_order order_details
+    order_details.reduce(0) do |total, order|
+      total + order.quantity * order.price
     end
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < Admin::AdminsController
   def index
-    @products = Product.recent_products.page(params[:page]).per(Settings.per_page)
+    @products = Product.recent_products
+                       .page(params[:page])
+                       .per(Settings.per_page_15)
   end
 end
