d7b657ee5990ae2f8a4e2e2ff28b0593bff9bd59
KhanhPV99 <khanhpv801@gmail.com>
2021-10-17 01:57:56 +0700

    sử dụng content_tag cho error message trong form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
     base_title = t "food_drink"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def show_message_for_field attribute
+    full_messages_for = @user.errors.full_messages_for(attribute)
+    return if full_messages_for.empty?
+
+    content_tag :ul, class: "error" do
+      full_messages_for.map{|msg| concat(content_tag(:li, msg))}
+    end
+  end
 end
